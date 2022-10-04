a97f5730eeb9b08039264f78b93f3eacc3e94559
mithunmajumdar <71630267+mithunmajumdar@users.noreply.github.com>
2022-10-04 13:48:51 +0530

    Basecode First Commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into books(bookid,bookname,author,price)
+values(5434,'Core and Advance Java','Robert Lafore',800);
+insert into books(bookid,bookname,author,price)
+values(0982,'Programming with Java','Joshua Bloch',350);
+insert into books(bookid,bookname,author,price)
+values(6321,'Data Structures and Algorithms in Java','Joshua Bloch Junior',600);
+insert into books(bookid,bookname,author,price)
+values(5433,'Effective Java','Mac Bloch Junior',500);
