c99d964668f83151477db329343a21867e6c00d0
mithunmajumdar <71630267+mithunmajumdar@users.noreply.github.com>
2022-10-04 14:57:44 +0530

    Basecode Commit

--- src/main/java/com/javatpoint/SpringBootRestJpa.java
+++ src/main/java/com/javatpoint/SpringBootRestJpa.java
@@ -2,10 +2,10 @@ package com.javatpoint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
-public class SpringBootCrudOperationApplication 
+public class SpringBootRestJpa 
 {
 public static void main(String[] args) 
 {
-SpringApplication.run(SpringBootCrudOperationApplication.class, args);
+SpringApplication.run(SpringBootRestJpa.class, args);
 }
 }
